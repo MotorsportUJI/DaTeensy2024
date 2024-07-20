9240ed9177d44d422ea30a1043b5c5fa5d181798
teo2peer <teo2din@gmail.com>
2024-07-20 20:38:07 +0200

    chore: Update MAX11610 constructor to include address, I2CPort, and TCA9548A

--- src/lib/Controller/ADC/MAX11610.cpp
+++ src/lib/Controller/ADC/MAX11610.cpp
@@ -1,18 +1,18 @@
 #include "MAX11610.h"
 
-MAX11610::MAX11610()
+MAX11610::MAX11610(uint8_t address, int I2CPort, TCA9548A *tca)
 {
-  _address = 0x34;
+  _address = address;
+  _I2CPORT = I2CPort;
+  _tca = tca;
+  // _address = 0x33;
 }
 
-void MAX11610::init(uint8_t address)
-{ 
-  _address = address;
-  Wire.begin();
-}
+
 
 void MAX11610::writeRegister(uint8_t reg, uint8_t value)
 {
+
   Wire.beginTransmission(_address);
   Wire.write(reg);
   Wire.write(value);
@@ -21,19 +21,39 @@ void MAX11610::writeRegister(uint8_t reg, uint8_t value)
 
 uint16_t MAX11610::readRegister(uint8_t reg)
 {
+
+
   Wire.beginTransmission(_address);
   Wire.write(reg);
   Wire.endTransmission(false);
 
-  Wire.requestFrom(_address, 2);
+  Wire.requestFrom(_address, static_cast<size_t>(2));
   uint16_t value = (Wire.read() << 8) | Wire.read();
+  Wire.endTransmission();
+  
   return value;
 }
 
 uint16_t MAX11610::readADC(uint8_t channel)
 {
+
+  _tca->pcaselect(_I2CPORT);
+  
   // The MAX11610 uses channel configuration register 0x08 to set the desired channel
   writeRegister(0x08, channel);
   // The ADC result is read from register 0x00 and 0x01 (16-bit data)
   return readRegister(0x00);
 }
+
+int MAX11610::readADC1(){  return readADC(0x01);  }
+int MAX11610::readADC0(){  return readADC(0x00);  }
+int MAX11610::readADC2(){  return readADC(0x02);  }
+int MAX11610::readADC3(){  return readADC(0x03);  }
+int MAX11610::readADC4(){  return readADC(0x04);  }
+int MAX11610::readADC5(){  return readADC(0x05);  }
+int MAX11610::readADC6(){  return readADC(0x06);  }
+int MAX11610::readADC7(){  return readADC(0x07);  }
+int MAX11610::readADC8(){  return readADC(0x08);  }
+int MAX11610::readADC9(){  return readADC(0x09);  }
+int MAX11610::readADC10(){  return readADC(0x0A);  }
+
--- src/lib/Controller/ADC/MAX11610.h
+++ src/lib/Controller/ADC/MAX11610.h
@@ -3,16 +3,30 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include "lib/Controller/I2CMux/TCA9548A.h"
 
 class MAX11610
 {
 public:
-    MAX11610();
-    void init(uint8_t address);
+    MAX11610(uint8_t address, int I2CPort, TCA9548A *tca);
     uint16_t readADC(uint8_t channel);
+    int readADC0();
+    int readADC1();
+    int readADC2();
+    int readADC3();
+    int readADC4();
+    int readADC5();
+    int readADC6();
+    int readADC7();
+    int readADC8();
+    int readADC9();
+    int readADC10();
+
 
 private:
     uint8_t _address;
+    int _I2CPORT =0;
+    TCA9548A *_tca;
 
     void writeRegister(uint8_t reg, uint8_t value);
     uint16_t readRegister(uint8_t reg);
